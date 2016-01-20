ae3729257570fc1f60a783985adbb19d314b608e
JScharm <student@BLUC02MG0K0FH51.local>
2016-01-20 10:26:16 -0600

    Initial Commit

---
+++ College Profile Builder/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  College Profile Builder
+//
+//  Created by JScharm on 1/20/16.
+//  Copyright © 2016 JScharm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+       
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+
+
+}
+
